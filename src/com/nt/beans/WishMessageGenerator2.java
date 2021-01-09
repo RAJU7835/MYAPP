d5148644f6ea1f77befabeb43c645db5383ed392
raju <rjdjkumar.java@gmail.com>
2021-01-09 17:35:31 +0530

    commit_4

---
+++ src/com/nt/beans/WishMessageGenerator2.java
@@ -0,0 +1,39 @@
+//WishMessageGenerator.java (Target Spring bean)
+package com.nt.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator2 {
+	 //bean property
+   private Date date;
+   
+   public WishMessageGenerator2() {
+	System.out.println("WishMessageGenerator::0-param constructor");
+	System.out.println("WishMessageGenerator::0-param constructor");
+}
+   
+   //setter method supporting setter Injection
+   public void setDate(Date date) {
+	   System.out.println("WishMessageGenerator.setDate()");
+	   this.date=date;
+   }
+   
+  //write  b.method having b.logic  using  Injected Date class object
+   
+   public  String  generateMessage(String user) {
+	   int hour=0;
+	   System.out.println("Injected date ::"+date);
+	   //get current hour of the day
+	   hour=date.getHours(); // 24 hrs format
+	   //generate wish message
+	   if(hour<12)
+		      return "Good Morning:::"+user;
+	   else if(hour<16)
+		   return "Good AfterNoon::"+user;
+	   else if(hour<20)
+		   return "Good Evening::"+user;
+	   else
+		   return "Good Night::"+user;
+   }
+   
+}
