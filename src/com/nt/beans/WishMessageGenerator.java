0dda5c411bc4b29c22d27bd2a500d991a5fac59e
raju <rjdjkumar.java@gmail.com>
2021-01-09 17:10:27 +0530

    commit_2

--- src/com/nt/beans/WishMessageGenerator.java
+++ src/com/nt/beans/WishMessageGenerator.java
@@ -9,6 +9,7 @@ public class WishMessageGenerator {
    
    public WishMessageGenerator() {
 	System.out.println("WishMessageGenerator::0-param constructor");
+	System.out.println("WishMessageGenerator::0-param constructor");
 }
    
    //setter method supporting setter Injection
